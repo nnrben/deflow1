ec818881d3eb9afc80deedf31403f540d48ba4ad
Crow <127041056+nnrben@users.noreply.github.com>
2026-03-08 18:42:57 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Providers } from './providers' // создадим ниже
+import Header from '@/components/layout/Header'
 
 const inter = Inter({ subsets: ['latin', 'cyrillic'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="ru">
       <body className={inter.className}>
         <Providers>
+            <Header />
           <main className="container mx-auto px-4 py-6">
             {children}
           </main>
