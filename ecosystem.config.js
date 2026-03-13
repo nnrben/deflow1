28ec62e8754aa7fb94a4fcb3cde9af362553eca4
ya <ya@example.com>
2026-03-13 22:27:52 +0500

    Add only src folder

--- ecosystem.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    apps: [
-        { name: 'next-app', script: 'npm', args: 'start' },
-        { name: 'news-collector', script: 'dist/services/news-collector.js' },
-        { name: 'forecast-generator', script: 'dist/services/forecast-generator.js' },
-        { name: 'trader-bot', script: 'dist/services/trader-bot.js' }
-    ]
-};
